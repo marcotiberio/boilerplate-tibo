39a354d42955e88883e47db31739307b2b7b69e3
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2026-01-14 15:34:40 +0100

    add new components

---
+++ Components/HeroHeader/functions.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Flynt\Components\HeroHeader;
+
+use Flynt\Utils\Asset;
+use Flynt\Utils\Options;
+use Flynt\FieldVariables;
+use Flynt\Shortcodes;
+use Flynt\ComponentManager;
+use Timber\Timber;
+
+function getACFLayout()
+{
+    return [
+        'name' => 'HeroHeader',
+        'label' => __('Hero Header', 'flynt'),
+        'sub_fields' => [
+            [
+                'label' => __('Content', 'flynt'),
+                'name' => 'contentTab',
+                'type' => 'tab',
+                'placement' => 'top',
+                'endpoint' => 0,
+            ],
+            [
+                'label' => __('Title', 'flynt'),
+                'name' => 'blockTitle',
+                'type' => 'text'
+            ],
+            [
+                'label' => __('Content', 'flynt'),
+                'name' => 'contentHtml',
+                'type' => 'wysiwyg',
+                'tabs' => 'visual',
+                'delay' => 1,
+                'media_upload' => 0,
+                'required' => 0,
+            ],
+            [
+                'label' => __('Button', 'flynt'),
+                'name' => 'buttonLink',
+                'type' => 'link',
+                'required' => 0,
+                'wrapper' => [
+                    'width' => 100
+                ],
+            ],
+            // [
+            //     'label' => __('Image', 'flynt'),
+            //     'name' => 'imageTab',
+            //     'type' => 'tab',
+            //     'placement' => 'top',
+            //     'endpoint' => 0,
+            // ],
+            // [
+            //     'label' => __('Background Image', 'flynt'),
+            //     'instructions' => __('Image-Format: JPG, PNG, SVG, WEBP.', 'flynt'),
+            //     'name' => 'backgroundImage',
+            //     'type' => 'image',
+            //     'preview_size' => 'medium',
+            //     'required' => 0,
+            //     'mime_types' => 'jpg,jpeg,png,svg,webp',
+            // ],
+            [
+                'label' => __('Options', 'flynt'),
+                'name' => 'optionsTab',
+                'type' => 'tab',
+                'placement' => 'top',
+                'endpoint' => 0
+            ],
+            [
+                'label' => '',
+                'name' => 'options',
+                'type' => 'group',
+                'layout' => 'row',
+                'sub_fields' => [
+                    FieldVariables\getColorBackground(),
+                    FieldVariables\getColorText(),
+                ]
+            ]
+        ]
+    ];
+}
