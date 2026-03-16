139ed5da9a4b07eb57e22ffb6b3d2175496c69b4
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2026-03-16 13:32:37 +0100

    fix labels

--- single-author.php
+++ single-author.php
@@ -1,7 +1,9 @@
 <?php
 
 use Timber\Timber;
+use Flynt\Utils\Options;
 
 $context = Timber::context();
+$context['backButtonLabel'] = Options::getTranslatable('Articles', 'backButtonLabel') ?: 'Zurück';
 
 Timber::render('templates/single-author.twig', $context);
