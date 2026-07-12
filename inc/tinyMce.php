2e694e0192b34df09fec93a465d963ac56ea321d
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2026-07-12 21:35:36 +0200

    partner logos background

--- inc/tinyMce.php
+++ inc/tinyMce.php
@@ -68,7 +68,7 @@ function getBlockFormats($blockFormats)
     if (!empty($blockFormats)) {
         $blockFormatStrings = array_map(
             function ($tag, $label) {
-                return "${label}=${tag}";
+                return "{$label}={$tag}";
             },
             $blockFormats,
             array_keys($blockFormats)
