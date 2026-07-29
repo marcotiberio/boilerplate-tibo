89da39cf5cbb64c56b1326435fe2a13d200ad5e2
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2026-07-29 09:19:22 +0200

    improve buttons and nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -67,7 +67,7 @@ module.exports = {
       sm: '640px',
       md: '780px',
       lg: '1024px',
-      lgplus: '1200px',
+      lgplus: '1320px',
       box: '1440px',
       xl: '1680px',
       max: '1920px'
